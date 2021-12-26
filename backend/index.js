b6671d5866128ccd0c0cc6d83f02d61b6bb4ba14
Rabia-Basari777 <rabia_fw10_200@masai.school>
2021-12-26 12:42:50 +0530

    complete chat app

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
 // Handling socket.io connections
 
-const io = require("socket.io")(2103);
+const io = require('socket.io')(8000);
 
 const users = {};
 
-io.on("connection", socket = {
-    socket.on("new-user-joined", name => {
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        // console.log("New user", name);
         users[socket.id] = name;
-        socket.broadcast.emit(name, "has joined the chat");
+        socket.broadcast.emit('user-joined', name);
     });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });  // shows the message to everyone except the sender
+    })
+
+    socket.on("disconnect", (message) => {
+        socket.broadcast.emit("left", users[socket.id]);
+        delete users[socket.id];
+    })
 })
---
+++ js/client.js
@@ -0,0 +1,39 @@
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById("sendForm");
+const messageInput = document.getElementById("messageInput");
+const messageContainer = document.querySelector(".container");
+let audio = new Audio('t2.mp3');
+
+
+const showNewUser = (message, position) => {
+    const msgElement = document.createElement("div");
+    msgElement.innerText = message;
+    msgElement.classList.add("message");
+    msgElement.classList.add(position);
+    messageContainer.append(msgElement);
+    audio.play();
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    showNewUser(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = "";
+})
+
+const name = prompt("Enter your name to join");
+socket.emit("new-user-joined", name);
+
+socket.on("user-joined", name => {
+    showNewUser(`${name} joined the chat`, 'right');
+})
+
+socket.on("receive", data => {
+    showNewUser(`${data.name}: ${data.message}`, 'left');
+})
+
+socket.on("left", name => {
+    showNewUser(`${name} left the chat`, 'left');
+})
